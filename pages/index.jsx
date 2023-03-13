755f4ea9687e23947646400dba3fb4829a0890b1
francis-aadatasolutions <francis@aadatasolutions.com>
2023-03-14 00:49:50 +0800

    changing images

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Future Analyticz</title>
+        <title>Future Analytics</title>
         <meta name='description' content='Future Analytics' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
